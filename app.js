bb80e8ff91e9b243b58e6d9ea58ad86f6da0d1e0
Aakashkhatri77 <aakash.kc.77@gmail.com>
2021-07-08 13:33:59 +0545

    cors library

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const db = require('./database/db')
+const cors = require('cors')
 const User_route = require('./routes/user_routes')
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
 app.use(User_route);
